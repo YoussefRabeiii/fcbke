2f7dcb3dfae2e9f9e59fbfb9b88cc03edb86e258
Youssef Rabei <youssefrabei2003@gmail.com>
2021-05-17 01:34:38 +0200

    ✨ Finished the Header

---
+++ components/Header.jsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+
+import {
+  BellIcon,
+  ChatIcon,
+  HomeIcon,
+  ViewGridIcon,
+  UserGroupIcon,
+  ChevronDownIcon,
+} from "@heroicons/react/solid";
+
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+
+import { HeaderIcon } from "@components/index";
+
+const Header = () => {
+  return (
+    <header className="sticky flex items-center p-2 lg:px-5 shadow-md top-0 z-50 bg-white">
+      {/* Search Section */}
+      <div className="flex items-center">
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600" />
+          <input
+            type="text"
+            className=" hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            placeholder="Search Facebook"
+          />
+        </div>
+      </div>
+
+      {/* Nav Section */}
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
+
+      {/* Profile Section */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* <Image src="" width={} height={} /> */}
+
+        {/* TODO: Use the real username */}
+        <p className="whitespace-nowrap font-semibold pr-3">Youssef Rabei</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Just In Time
+  mode: "jit",
+
+  // Remove all unused styles
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: [],
+};
